56813efab9e20163d420b62828cd30b1e23cd0a5
吴忠 <wuzhong1598@qq.com>
2018-11-02 09:22:12 +0800

    commit code

--- src/hello.js
+++ src/hello.js
@@ -1,2 +1,2 @@
-const hello = () => 'hello world nicex.xx'
+const hello = () => 'hello world nice....'
 export default hello
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import hello from './hello.js'
 const div = document.createElement('div')
 div.innerHTML = hello()
 
+if(module.hot) {
+    module.hot.accept('./hello.js', function() {
+        div.innerHTML = hello()
+    })
+}
+
 document.body.appendChild(div)
